11545863cac07aa258e56d08e7c3a8c09b8858a6
Dylan Hays <twentygx@outlook.com>
2022-07-20 09:25:26 -0500

    commit tree bc94a12c3808f42d856a4ee88aca693f3e26dd0f

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((tut) => {
+    const words = tut.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+    }
+    let returnStr = words.join(" ");
+    return returnStr;
+  })
 }
